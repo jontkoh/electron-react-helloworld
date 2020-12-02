f2ae9f823a8cc3c6aa6ea922b72e51025cdeee18
Jonathan Koh <jontkoh@gmail.com>
2020-12-01 23:36:06 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+const name = "user";
+const element = <h1>hello, {name}</h1>;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {element}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
